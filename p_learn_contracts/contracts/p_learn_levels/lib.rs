dacb1c05d584704c092603602069cb10bff25d9a
Emanherawy <eman.herawy@gmail.com>
2023-10-28 05:01:27 +0300

    add extension : mintable, burnable, access control , batch and enumerable

--- p_learn_contracts/contracts/p_learn_levels/lib.rs
+++ p_learn_contracts/contracts/p_learn_levels/lib.rs
@@ -1,10 +1,11 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
         
-#[openbrush::implementation(PSP37)]
+#[openbrush::implementation(PSP37, PSP37Batch, PSP37Burnable, PSP37Mintable, PSP37Enumerable,AccessControl)]
 #[openbrush::contract]
 pub mod p_learn_levels {
         use openbrush::{
+        modifiers,
         traits::Storage,
     };
     #[ink(storage)]
@@ -12,13 +13,31 @@ pub mod p_learn_levels {
     pub struct Levels {
      #[storage_field]
         psp37: psp37::Data,
-       
+        #[storage_field]
+        enumerable: enumerable::Data,
+         #[storage_field]
+        access: access_control::Data,
     }
-   
+     const MINTER: RoleType = ink::selector_id!("MINTER");
+   #[default_impl(PSP37Burnable)]
+    #[modifiers(only_role(MINTER))]
+    fn burn() {}
+
+    #[default_impl(PSP37Mintable)]
+    #[modifiers(only_role(MINTER))]
+    fn mint() {}
     impl Levels {
-        #[ink(constructor)]
-        pub fn new() -> Self {
-            Self::default()
+       #[ink(constructor)]
+              pub fn new(minter: AccountId) -> Self {
+            let mut instance = Self::default();
+
+            let caller = instance.env().caller();
+            access_control::Internal::_init_with_admin(&mut instance, Some(caller));
+            // We grant minter role to caller in constructor, so he can mint/burn tokens
+            AccessControl::grant_role(&mut instance, MINTER, Some(caller)).expect("Should grant MINTER role");
+            AccessControl::grant_role(&mut instance, MINTER, Some(minter)).expect("Should grant MINTER role");
+
+            instance
         }
     }
 }
