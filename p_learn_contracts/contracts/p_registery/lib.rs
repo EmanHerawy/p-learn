cefbedb40ddb96af7731eac4fda8cf8577e525fc
Emanherawy <eman.herawy@gmail.com>
2023-10-29 02:52:52 +0300

    init contract for issuing  level nft and manage players

---
+++ p_learn_contracts/contracts/p_registery/lib.rs
@@ -0,0 +1,35 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+#[openbrush::implementation(AccessControl)]
+
+#[openbrush::contract]
+mod p_registry {
+     use openbrush::{
+        modifiers,
+        traits::Storage,
+    };
+    /// Defines the storage of your contract.
+    /// Add new fields to the below struct in order
+    /// to add new static storage fields to your contract.
+    #[ink(storage)]
+    #[derive(Default, Storage)]
+    pub struct Contract {
+        /// Stores a single `bool` value on the storage.
+         #[storage_field]
+        access: access_control::Data,
+    }
+
+    impl Contract {
+        /// Constructor that initializes the `bool` value to the given `init_value`.
+        #[ink(constructor)]
+        pub fn new() -> Self {
+             let mut instance = Self::default();
+
+            let caller = instance.env().caller();
+            access_control::Internal::_init_with_admin(&mut instance, Some(caller));
+              instance
+        }
+
+      
+    }
+
+    }
