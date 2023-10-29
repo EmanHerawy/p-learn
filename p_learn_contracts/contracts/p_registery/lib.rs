1e90a8e71f10caaa79cd01695926192e9776b362
Emanherawy <eman.herawy@gmail.com>
2023-10-29 03:12:51 +0300

    add mapping to keep track of player last level

--- p_learn_contracts/contracts/p_registery/lib.rs
+++ p_learn_contracts/contracts/p_registery/lib.rs
@@ -4,16 +4,19 @@
 #[openbrush::contract]
 mod p_registry {
      use openbrush::{
-        modifiers,
+        // modifiers,
         traits::Storage,
     };
+    use ink::storage::Mapping;
+    // use ink::ink_primitives::AccountId;
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
     #[ink(storage)]
     #[derive(Default, Storage)]
     pub struct Contract {
-        /// Stores a single `bool` value on the storage.
+        /// Stores a `mapping` for last level per player on the storage.
+         player_last_level: Mapping<AccountId, u32>,
          #[storage_field]
         access: access_control::Data,
     }
